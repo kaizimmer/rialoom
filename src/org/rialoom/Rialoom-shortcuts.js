02c4456bab93fee12b380a4a05d9f330585aafb8
kaizimmer <hawkmoon76@gmx.de>
2012-06-24 15:12:20 +0200

    Add ria shortcut

---
+++ src/org/rialoom/Rialoom-shortcuts.js
@@ -0,0 +1,32 @@
+/**
+* Copyright (c) 2012 Kai Zimmer [http://www.kaizimmer.com]
+*
+* Permission is hereby granted to use, modify, and distribute this file
+* in accordance with the terms of the license agreement accompanying it.
+*/
+if ( this.ria == null )
+{
+	this.ria = org.rialoom;
+	// inheritance
+	ria.createPackage = function ( packagePath ) { return org.rialoom.utils.ClassUtils.createPackage(packagePath) };
+	ria.createConstructor = function ( packagePath, className ) { return org.rialoom.utils.ClassUtils.createConstructor(packagePath, className) };
+	ria.inherits = function ( subClass, superClass ) { return org.rialoom.utils.ClassUtils.inherits( subClass, superClass ) };
+	// debugging
+	ria.log = function ( message ) { return org.rialoom.utils.Debug.log.apply(org.rialoom.utils.Debug, arguments); };
+	ria.activateLogging = function ( ) { return org.rialoom.utils.Debug.activateLogging.apply(org.rialoom.utils.Debug); };
+	ria.disableLogging = function ( ) { return org.rialoom.utils.Debug.disableLogging.apply(org.rialoom.utils.Debug); };
+	// delegation
+	/**
+	 * Delegates method calls to passed handler. Handler scope is defined by passed scope argument.
+	 * @param scope : Object
+	 * @param handler : Function
+	 * @return delegate : Function
+	 */
+	ria.createDelegate = function ( scope, handler ) { return org.rialoom.utils.Delegate.create(scope, handler) };
+	/**
+	 * Returns internal [[Class]] property of passed instance. Returns "_global" if the passed argument is global scope.
+	 * @param instance : Object
+	 * @return String representation of instance's internal [[Class]].
+	 */
+	ria.getTypeOf = function ( instance ) { return org.rialoom.utils.ClassUtils.getTypeOf(instance); };
+}
